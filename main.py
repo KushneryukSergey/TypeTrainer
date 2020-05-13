75e5e2a7584f2f46f4534650fd8735046c96f84f
KushneryukSergey <kushneriuk.ss@phystech.edu>
2020-05-13 15:39:47 +0500

    few changes

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from module.action.game_options import *
-
-
 def main():
-    init_game()
-    game = Game()  # by default game starts in main menu, cut-scene at first play can be added
-    while 1:  # main cycle of program
-        game.process()
+    pass
 
 
 if __name__ == "__main__":
